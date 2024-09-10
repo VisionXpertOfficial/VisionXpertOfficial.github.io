d347aab506536b3d9468a4d489b246e643069bce
VisionXpertOfficial <contact@vxofficial.com>
2024-09-10 15:19:34 +0100

    initial commit

--- app.js
+++ app.js
@@ -4,18 +4,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const images = document.querySelectorAll("img");
 
+    const apiKey = 'YOUR_ACCESS_KEY';
+    const query = 'technology';
+
     for (const image of images) {
         // console.log(image)
-        fetch("https://dog.ceo/api/breeds/image/random")
+        fetch(`https://api.unsplash.com/photos/random?query=${query}&client_id=${apiKey}`)
         .then(Response => Response.json())
         .then(data => {
-           image.src = data.message
+           image.src = data.urls.small;
            image.width = 100;
            image.height = 100;
     })
+    .catch(err => console.error(err));
 
 }
-
-
 
 })
